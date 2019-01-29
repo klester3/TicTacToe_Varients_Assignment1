351179977a51696a0a679899546304f6e911abd5
Kyle Lester <klester3@my.apsu.edu>
2019-01-28 23:56:46 -0600

    Added Random Turn class, and layout files.

---
+++ app/src/main/java/com/kyle_jason_group/tic_tac_toe_varients_assignment1/RandomTurnActivity.java
@@ -0,0 +1,18 @@
+package com.kyle_jason_group.tic_tac_toe_varients_assignment1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+public class RandomTurnActivity extends Activity implements View.OnClickListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
