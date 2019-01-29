01e447882e296056dd5b6151d3801630cd01511d
Kyle Lester <klester3@my.apsu.edu>
2019-01-29 01:05:14 -0600

    Added image buttons for board, test button, and just basic layout.

--- app/src/main/java/com/kyle_jason_group/tic_tac_toe_varients_assignment1/MainActivity.java
+++ app/src/main/java/com/kyle_jason_group/tic_tac_toe_varients_assignment1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.kyle_jason_group.tic_tac_toe_varients_assignment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Kyle Test Button
+        findViewById(R.id.button_kyle_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),
+                        RandomTurnActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
